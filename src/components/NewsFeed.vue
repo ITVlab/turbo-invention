083f7f597133e06d934722456554e59337faeb4c
Anthony Garera <anthonygarera@anthonys-mbp.lan>
2018-10-07 20:35:07 -0400

    Mixed content fix

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -41,7 +41,7 @@ export default {
   async created() {
     try {
       const response = await axios.get(
-        `http://data.androidtv.news/wp-json/wp/v2/posts`
+        `https://data.androidtv.news/wp-json/wp/v2/posts`
       );
       this.posts = response.data;
     } catch (e) {
